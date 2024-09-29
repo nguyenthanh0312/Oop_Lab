a0e948692fb3bb3bef9fbb29e539a7bb3c4a374e
nguyenthanh0312 <150718172+nguyenthanh0312@users.noreply.github.com>
2024-09-29 15:41:34 +0700

    Update triangle_6_3.java

--- triangle_6_3.java
+++ triangle_6_3.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class triangle_6_3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the height of the triangle (n): ");
+        System.out.print("Enter the height of the triangle: ");
         int n = scanner.nextInt();
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n - i; j++) {
